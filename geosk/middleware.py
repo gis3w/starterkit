be56fc228b1cd6960af78659edc4003d99d408f5
Walter Lorenzetti <lorenzetti@gis3w.it>
2018-09-11 09:23:59 +0200

    Continue BF GUI modifications.

--- geosk/middleware.py
+++ geosk/middleware.py
@@ -5,6 +5,8 @@ from re import compile
 EXEMPT_URLS = [compile(settings.LOGIN_URL.lstrip('/'))]
 if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
     EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
+if hasattr(settings, 'STATIC_URL'):
+    EXEMPT_URLS += [compile(settings.STATIC_URL.lstrip('/'))]
 
 
 class LoginRequiredMiddleware(object):
